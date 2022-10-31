af917fef28e40336f15cbda29ab08d32963ef1ef
Tomas Halada <halada.tms@gmail.com>
2022-10-31 14:34:15 +0100

    working on SPH dambreak demo

---
+++ SPHdemo/SPHFullDemo/ParticlesConfig.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <TNL/Containers/StaticVector.h>
+#include <TNL/Algorithms/Segments/CSR.h>
+#include <TNL/Algorithms/Segments/Ellpack.h>
+
+#include "../../Particles/GenerateCellIndex.h"
+
+namespace TNL {
+namespace ParticleSystem {
+
+class ParticleSystemConfig
+{
+  public:
+
+  using GlobalIndexType = int;
+  using LocalIndexType = int;
+  using CellIndexType = int;
+  using RealType = float;
+
+  static constexpr int spaceDimension = 2;
+  //static constexpr int numberOfParticles = 21001;
+  static constexpr int numberOfParticles = 301;
+  static constexpr int maxOfNeigborsPerParticle = 150;
+
+  using CoordinatesType = Containers::StaticVector< 2, int >;
+
+  static constexpr RealType searchRadius = 0.1*2;
+  static constexpr int gridXsize = 20;
+  static constexpr int gridYsize = 20;
+
+  //static constexpr CoordinatesType origin = {0, 0}; //.. I would like something like this
+  static constexpr int gridXbegin = 0;
+  static constexpr int gridYbegin = 0;
+
+  // ... set particle system ...
+
+  using DeviceType = Devices::Host;
+  using CellIndexerType = SimpleCellIndex<ParticleSystemConfig, DeviceType>; //?
+
+
+  using NeighborListType = typename Algorithms::Segments::Ellpack< TNL::Devices::Host, int >;
+};
+
+} //namespace ParticleSystem
+} //namespace TNL
---
+++ SPHdemo/SPHFullDemo/SPHDemoTestFull.cu
@@ -0,0 +1,118 @@
+#include <iostream>
+
+#include <TNL/Devices/Host.h>
+#include <TNL/Devices/Cuda.h>
+
+#include "../../Particles/Particles.h"
+
+//#include "../../ParticlesConfig.h"
+#include "ParticlesConfig.h"
+
+#include "../../Particles/neighbourSearch.h"
+
+#include "../../SPH/SPH.h"
+#include "../../SPH/SPHFluidVariables.h"
+#include "../../SPH/SPHConfig.h"
+#include "../../SPH/SPHInteractions.h"
+
+#include "../../Readers/VTKReader.h"
+
+using namespace TNL;
+
+int main( int argc, char* argv[] )
+{
+
+  /**
+   * Number of particles
+   */
+  using ParticlesConfig = ParticleSystemConfig;
+  using Device = Devices::Host;
+
+  using ParticleSystem = typename ParticleSystem::Particles< ParticlesConfig, Device >;
+  using Variables = typename TNL::ParticleSystem::SPH::SPHFluidVariables< TNL::ParticleSystem::SPH::SPHFluidConfig < Device > >;
+  using NeighborSearch = typename TNL::ParticleSystem::NeighborSearch< ParticlesConfig, ParticleSystem >;
+
+  using SPHModel = typename TNL::ParticleSystem::SPH::WCSPH_DBC< ParticleSystem, TNL::ParticleSystem::SPH::SPHFluidConfig < Device >>;
+  using SPHSimulation = typename TNL::ParticleSystem::SPH::SPHSimulation< SPHModel, ParticleSystem, NeighborSearch >;
+
+  using Reader = TNL::ParticleSystem::Readers::VTKReader;
+
+  /**
+   * Create the simulation.
+   */
+  SPHSimulation mySPHSimulation( ParticlesConfig::numberOfParticles, ParticlesConfig::searchRadius, ParticlesConfig::gridXsize * ParticlesConfig::gridYsize );
+
+  /***
+   * Read the particle file.
+   */
+  const std::string inputFileName="./CaseDamBreak_DEMO.vtk";
+  Reader myReader( inputFileName );
+  myReader.detectParticleSystem();
+  myReader.template loadParticle< ParticleSystem >( mySPHSimulation.particles );
+
+  /***
+   * Setup type for boundary particles and initial condition.
+   */
+  for( unsigned int p = 0; p < mySPHSimulation.particles.getNumberOfParticles(); p ++ )
+  {
+
+    if( ( mySPHSimulation.particles.getPoint( p )[ 0 ] == 0. ) ||
+        ( mySPHSimulation.particles.getPoint( p )[ 0 ] == 4. ) ||
+        ( mySPHSimulation.particles.getPoint( p )[ 1 ] == 0. ) )
+    {
+      mySPHSimulation.model.vars.type[ p ] == 1.;
+    }
+    else
+    {
+      mySPHSimulation.model.vars.type[ p ] == 0.;
+
+    }
+
+      mySPHSimulation.model.vars.rho[ p ] == 1000.;
+      mySPHSimulation.model.vars.rho[ p ] == 1000.;
+      mySPHSimulation.model.vars.v[ p ] == 0.;
+
+  }
+
+  //std::cout << "mySPHSimulation particle positions: " << mySPHSimulation.model.points << std::endl;
+  //:info: std::cout << "Grid informations: " << std::endl;
+  //:info: mySPHSimulation.particles.GetParticlesInformations();
+
+  //:debug: /**
+  //:debug:  * Find neighbors within the SPH simulation - step by step.
+  //:debug:  */
+  //:debug: mySPHSimulation.particles.computeGridCellIndices();
+  //:debug: mySPHSimulation. particles.computeParticleCellIndices();
+  //:debug: //model.sortParticlesAndVariables();
+  //:debug: mySPHSimulation.particles.sortParticles();
+  //:debug: std::cout << "Grid cell indices: " << mySPHSimulation.particles.getGridCellIndices() << std::endl;
+  //:debug std::cout << "Particle cell indices: " << mySPHSimulation.particles.getParticleCellIndices() << std::endl;
+
+
+  /**
+   * Find neighbors within the SPH simulation.
+   */
+  std::cout << "\nFind neighbros within the SPH simulation." << std::endl;
+  mySPHSimulation.PerformNeighborSearch();
+
+  //:: /**
+  //::  * Test the loop over particle neighbors.
+  //::  */
+  //:: std::cout << "\nTest the loop over particle neighbros." << std::endl;
+  //:: mySPHSimulation.Interact();
+  //:: std::cout << "mySPHSimulation inices-types: " << mySPHSimulation.model.vars.type << std::endl;
+  //:: std::cout << "mySPHSimulation pressure field: " << mySPHSimulation.model.vars.p << std::endl;
+
+  //:: /**
+  //::  * Test particle positions -> model points bridge.
+  //::  */
+  //:: std::cout << "\nTest particle positions -> model points bridge." << std::endl;
+  //:: std::cout << mySPHSimulation.model.points << std::endl; //For some reason this works,...
+  //:: std::cout << "Particle points bridge - point with idx 1: " << mySPHSimulation.model.points[1] << std::endl;
+
+
+  std::cout << "\nDone ... " << std::endl;
+
+
+}
+
