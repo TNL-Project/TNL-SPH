17964811e9870a9ced7bee067e48269e25559f21
Tomáš Halada <halada.tms@gmail.com>
2022-12-14 20:31:37 +0100

    fix messed up editorconfig settings

--- Particles/neighbourSearch.h
+++ Particles/neighbourSearch.h
@@ -14,114 +14,112 @@ class NeighborSearch
 public:
 
   /* common */
-  using DeviceType = typename ParticleSystem::Device; //mh
-	using ParticlePointer = typename Pointers::SharedPointer< ParticleSystem, DeviceType >;
+   using DeviceType = typename ParticleSystem::Device; //mh
+   using ParticlePointer = typename Pointers::SharedPointer< ParticleSystem, DeviceType >;
 
-  using LocalIndexType = typename ParticleSystem::LocalIndexType;
-  using GlobalIndexType = typename ParticleSystem::GlobalIndexType;
-  using RealType = typename ParticleSystem::RealType;
+   using LocalIndexType = typename ParticleSystem::LocalIndexType;
+   using GlobalIndexType = typename ParticleSystem::GlobalIndexType;
+   using RealType = typename ParticleSystem::RealType;
 
-	using CellIndexType = typename ParticleSystem::CellIndexType;
-  using CellIndexArrayType = typename ParticleSystem::CellIndexArrayType;
-	using CellIndexArrayView = typename Containers::ArrayView< typename ParticleSystem::CellIndexType, DeviceType >;
+   using CellIndexType = typename ParticleSystem::CellIndexType;
+   using CellIndexArrayType = typename ParticleSystem::CellIndexArrayType;
+   using CellIndexArrayView = typename Containers::ArrayView< typename ParticleSystem::CellIndexType, DeviceType >;
 
-  /**
-   * Constructors.
-   */
-  NeighborSearch(ParticlePointer& particles, GlobalIndexType cellCount)
-  : particles(particles), firstCellParticle(cellCount), lastCellParticle(cellCount)
-  {
-    //firstCellParticle = -1;
-    //lastCellParticle = -1;
+   /**
+    * Constructors.
+    */
+   NeighborSearch(ParticlePointer& particles, GlobalIndexType cellCount)
+   : particles(particles), firstCellParticle(cellCount), lastCellParticle(cellCount)
+   {
+      //firstCellParticle = -1;
+      //lastCellParticle = -1;
 
-    firstCellParticle = INT_MAX;
-    lastCellParticle = INT_MAX;
+      firstCellParticle = INT_MAX;
+      lastCellParticle = INT_MAX;
+   }
 
-  }
+   /**
+    * Get list of first particle in cells.
+    */
+   const typename ParticleSystem::CellIndexArrayType& // -> using..
+   getCellFirstParticleList() const;
 
-  /**
-   * Get list of first particle in cells.
-   */
-  const typename ParticleSystem::CellIndexArrayType& // -> using..
-  getCellFirstParticleList() const;
+   typename ParticleSystem::CellIndexArrayType& // -> using..
+   getCellFirstParticleList();
 
-  typename ParticleSystem::CellIndexArrayType& // -> using..
-  getCellFirstParticleList();
+   /**
+    * Get list of last particle in cells.
+    */
+   const typename ParticleSystem::CellIndexArrayType& // -> using..
+   getCellLastParticleList() const;
 
-  /**
-   * Get list of last particle in cells.
-   */
-  const typename ParticleSystem::CellIndexArrayType& // -> using..
-  getCellLastParticleList() const;
+   typename ParticleSystem::CellIndexArrayType& // -> using..
+   getCellLastParticleList();
 
-  typename ParticleSystem::CellIndexArrayType& // -> using..
-  getCellLastParticleList();
+   /**
+    * Assign to each cell index of first contained particle.
+    */
+   __cuda_callable__
+   void
+   particlesToCells();
 
-  /**
-   * Assign to each cell index of first contained particle.
-   */
-  __cuda_callable__
-  void
-  particlesToCells();
+   /**
+    * Runs cycle over all particles, for each particles run cycle over ints negihbors
+    * and exec given function f for each pair.
+    */
+   template< typename Function, typename... FunctionArgs >
+   __cuda_callable__
+   void
+   loopOverParticlesAndNeighbors( Function f, FunctionArgs... args  ); //rename this
 
-	/**
-	 * Runs cycle over all particles, for each particles run cycle over ints negihbors
-	 * and exec given function f for each pair.
-	 */
-	template< typename Function, typename... FunctionArgs >
-  __cuda_callable__
-  void
-	loopOverParticlesAndNeighbors( Function f, FunctionArgs... args  ); //rename this
+   /**
+    * Loop that for given particle i runs cycle over all its neighbors
+    * and exec given function f for each pair.
+    * TODO: Improve the interface and argument handling.
+    */
+   template< typename Function, typename... FunctionArgs >
+   __cuda_callable__
+   void
+   loopOverNeighbors( const GlobalIndexType i, const GlobalIndexType numberOfParticles, const CellIndexArrayView view_firstCellParticle, const CellIndexArrayView view_particleCellIndex, Function f, FunctionArgs... args );
 
-	/**
-	 * Loop that for given particle i runs cycle over all its neighbors
-	 * and exec given function f for each pair.
-	 * TODO: Improve the interface and argument handling.
-	 */
-	template< typename Function, typename... FunctionArgs >
-  __cuda_callable__
-  void
-	loopOverNeighbors( const GlobalIndexType i, const GlobalIndexType numberOfParticles, const CellIndexArrayView view_firstCellParticle, const CellIndexArrayView view_particleCellIndex, Function f, FunctionArgs... args );
+   /**
+    * For all particles run loop over neighbors and assemble neighbor list.
+     * TEMP: This function is actually not used, but show the structure of negihbor loop.
+    */
+   void
+   searchForNeighborsExampleLoop();
 
-  /**
-   * For all particles run loop over neighbors and assemble neighbor list.
-	 * TEMP: This function is actually not used, but show the structure of negihbor loop.
-   */
-  void
-  searchForNeighborsExampleLoop();
+   /**
+    * Runs all necessary functions and fills neighbor list.
+    */
+   void
+   searchForNeighbors();
 
-  /**
-   * Runs all necessary functions and fills neighbor list.
-   */
-  void
-  searchForNeighbors();
+   /**
+    * Search for neighbors and assemble neighbor list.
+     * TEMP: This function has some limitations, but show how the neighbor loop should work.
+    */
+   void
+   searchForNeighborsWithForAll();
 
-  /**
-   * Search for neighbors and assemble neighbor list.
-	 * TEMP: This function has some limitations, but show how the neighbor loop should work.
-   */
-  void
-  searchForNeighborsWithForAll();
+   /**
+    * Search for neighbors and assemble neighbor list.
+     */
+   void
+   searchForNeighborsWithForEach();
 
-  /**
-   * Search for neighbors and assemble neighbor list.
-	 */
-  void
-  searchForNeighborsWithForEach();
-
-  /**
-   * Reset the list with first and last particle in cell.
-   */
-	void
-  resetListWithIndices(); //protected?
-
+   /**
+    * Reset the list with first and last particle in cell.
+    */
+   void
+   resetListWithIndices(); //protected?
 
 protected:
 
-  ParticlePointer particles;
+   ParticlePointer particles;
 
-  CellIndexArrayType firstCellParticle;
-  CellIndexArrayType lastCellParticle;
+   CellIndexArrayType firstCellParticle;
+   CellIndexArrayType lastCellParticle;
 
 };
 
