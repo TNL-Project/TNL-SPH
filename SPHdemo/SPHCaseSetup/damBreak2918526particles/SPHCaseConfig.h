d0a8b58999b13ec2c4c89f4fb66cafa6def98fde
Tomáš Halada <halada.tms@gmail.com>
2022-12-30 16:32:57 +0100

    add damBreak2918526particles testcase

---
+++ SPHdemo/SPHCaseSetup/damBreak2918526particles/SPHCaseConfig.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace TNL {
+namespace ParticleSystem {
+namespace SPH {
+
+template< typename Device>
+class SPHCaseConfig
+{
+   public:
+   using DeviceType = Device;
+
+   static constexpr int spaceDimension = 2;
+
+   using GlobalIndexType = int;
+   using LocalIndexType = int;
+   using CellIndexType = int;
+   using RealType = float;
+
+   static constexpr float mass = 0.0000625f;
+   static constexpr float speedOfSound = 34.3f;
+   static constexpr float coefB = 168031.3f;
+   static constexpr float rho0 = 1000.f;
+   static constexpr float h = 0.000354f;
+   static constexpr float delta = 0.1f;
+   static constexpr float alpha = 0.02f;
+   static constexpr float eps = 0.001f;
+   static constexpr float dtInit = 0.000002f; //5f
+};
+
+} //SPH
+} //namespace ParticleSystem
+} //namespace TNL
+
