375ceba1985cd666ab2280502355bbdc87471024
Tomáš Halada <halada.tms@gmail.com>
2023-05-29 08:35:17 +0200

    Add draft for SPH physical object.

---
+++ SPH/PhysicalObject.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "SPHTraits.h"
+#include <thrust/sort.h>
+#include <thrust/execution_policy.h>
+#include <thrust/gather.h>
+
+namespace TNL {
+namespace ParticleSystem {
+namespace SPH {
+
+class PhysicalObjectMetada
+{
+
+};
+
+template< typename ParticleSystem, typename NeighborSearch, typename SPHCaseConfig, typename Variables, typename IntegratorVariables >
+class PhysicalObject
+{
+   public:
+   using DeviceType = typename ParticleSystem::Device;
+   using ParticlePointer = typename Pointers::SharedPointer< ParticleSystem, DeviceType >;
+   using NeighborSearchPointer = typename Pointers::SharedPointer< NeighborSearch, DeviceType >;
+   using VariablesPointer = typename Pointers::SharedPointer< Variables, DeviceType >;
+   using IntegratorVariablesPointer = typename Pointers::SharedPointer< IntegratorVariables, DeviceType >;
+
+   using SPHTraitsType = SPHFluidTraits< SPHCaseConfig >;
+   using GlobalIndexType = typename SPHTraitsType::GlobalIndexType;
+   using RealType = typename SPHTraitsType::RealType;
+
+   using IndexArrayType = typename SPHTraitsType::IndexArrayType;
+   using IndexArrayTypePointer = typename Pointers::SharedPointer< IndexArrayType, DeviceType >;
+
+   PhysicalObject( GlobalIndexType size, GlobalIndexType sizeAllocated, RealType h, GlobalIndexType numberOfCells )
+   : particles( size, sizeAllocated, h ), neighborSearch( particles, numberOfCells ), variables( sizeAllocated ),
+     integratorVariables( sizeAllocated ), sortPermutations( sizeAllocated ), points_swap( sizeAllocated ) {};
+
+   ParticlePointer&
+   getParticles()
+   {
+      return this->particles;
+   }
+
+   const ParticlePointer&
+   getParticles() const
+   {
+      return this->particles;
+   }
+
+   virtual VariablesPointer&
+   getVariables()
+   {
+      return this->variables;
+   }
+
+   virtual const VariablesPointer&
+   getVariables() const
+   {
+      return this->variables;
+   }
+
+
+   void sortParticles()
+   {
+      GlobalIndexType numberOfParticle = particles->getNumberOfParticles();
+      auto view_particleCellIndices = particles->getParticleCellIndices().getView();
+      auto view_map = sortPermutations->getView();
+
+      sortPermutations->forAllElements( [] __cuda_callable__ ( int i, int& value ) { value = i; } );
+      thrust::sort_by_key( thrust::device, view_particleCellIndices.getArrayData(),
+            view_particleCellIndices.getArrayData() + numberOfParticle, view_map.getArrayData() ); //TODO: replace thrust::device
+
+      auto view_points = particles->getPoints().getView();
+#ifdef PREFER_SPEED_OVER_MEMORY
+      auto view_points_swap = points_swap.getView();
+      thrust::gather( thrust::device, view_map.getArrayData(), view_map.getArrayData() + numberOfParticle,
+            view_points.getArrayData(), view_points_swap.getArrayData() );
+      particles->getPoints().swap( points_swap );
+#else
+      //TODO: Error or implement.
+#endif
+      variables->sortVariables( sortPermutations, particles->getNumberOfParticles() );
+      integratorVariables->sortVariables( sortPermutations, particles->getNumberOfParticles() );
+   }
+
+   ParticlePointer particles;
+   NeighborSearchPointer neighborSearch;
+   VariablesPointer variables;
+   IntegratorVariablesPointer integratorVariables;
+
+   IndexArrayTypePointer sortPermutations;
+
+#ifdef PREFER_SPEED_OVER_MEMORY
+   using PointArrayType = typename ParticleSystem::PointArrayType;
+   PointArrayType points_swap;
+#endif
+};
+
+}
+}
+}
+
