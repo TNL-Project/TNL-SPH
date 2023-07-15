402f5974d2ac33d6ce3e88fdb162b07724e01bc7
Tomáš Halada <halada.tms@gmail.com>
2023-07-15 19:07:14 +0200

    Remove deprecated files.

--- SPHdemo/SPHCases/SPHCase.cu
+++
@@ -1,8 +0,0 @@
-//#include "damBreak2D_WCSPH-DBC/CaseSetup.h"
-#include "damBreak2D_WCSPH-BI/CaseSetup.h"
-//#include "damBreak2D_RSPH/CaseSetup.h"
-
-//#include "simpleInlet2D_WCSPH-DBC_test/CaseSetup.h"
-//#include "sifon2D_WCSPH-DBC/CaseSetup.h"
-//#include "damBreak3D_WCSPH-DBC/CaseSetup.h"
-//#include "openChannel2D_WCSPH-DBC/CaseSetup.h"
