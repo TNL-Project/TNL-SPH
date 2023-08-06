a5a8f84fbc37e98a5fe905c5d631f63dfcecb591
Tomáš Halada <halada.tms@gmail.com>
2023-08-06 23:45:16 +0200

    Added BC types.

---
+++ SPH/Models/WCSPH_DBC/BoundaryConditionsTypes.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace TNL {
+namespace ParticleSystem {
+namespace SPH {
+namespace WCSPH_BCTypes {
+
+struct DBC
+{};
+
+struct MDBC
+{};
+
+struct MDBCVelocity //not implement yet
+{};
+
+struct GWBC //not implemented
+{};
+
+struct OpenBoundary
+{};
+
+} // WCSPHSolidWallBCTypes
+} // SPH
+} // ParticleSystem
+} // TNL
+
