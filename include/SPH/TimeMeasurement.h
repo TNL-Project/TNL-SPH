4f3329ba9560eec34d7ed30ccb93da157c8e8686
Tomáš Halada <halada.tms@gmail.com>
2023-11-14 22:54:43 +0100

    Updated time measurement.

---
+++ include/SPH/TimeMeasurement.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "TNL/Timer.h"
+#include <map>
+
+namespace TNL {
+namespace ParticleSystem {
+namespace SPH {
+
+template< typename SPHConfig >
+class TimerMeasurement
+{
+public:
+
+   using SPHTraitsType = SPHFluidTraits< SPHConfig >;
+   using GlobalIndexType = typename SPHTraitsType::GlobalIndexType;
+   using RealType = typename SPHTraitsType::RealType;
+
+   TimerMeasurement()
+   {
+      timers.insert( { "search", TNL::Timer()  } );
+      timers.insert( { "search_reset", TNL::Timer()  } );
+      timers.insert( { "search_cellIndices", TNL::Timer()  } );
+      timers.insert( { "search_sort", TNL::Timer()  } );
+      timers.insert( { "search_toCells", TNL::Timer()  } );
+      timers.insert( { "interact", TNL::Timer()  } );
+      timers.insert( { "integrate", TNL::Timer()  } );
+      timers.insert( { "total", TNL::Timer()  } );
+   }
+
+   void
+   addTimer( const std::string keyword )
+   {
+      timers.insert( { keyword, TNL::Timer() } );
+   }
+
+   void
+   start( const std::string keyword )
+   {
+      this->timers[ keyword ].start();
+   }
+
+   void
+   stop( const std::string keyword )
+   {
+      this->timers[ keyword ].stop();
+   }
+
+   void
+   print( const GlobalIndexType stepsTotal )
+   {
+      std::ostream &str = std::cout;
+      TNL::Logger logger( 100, str );
+      logger.writeHeader( "Computation time" );
+      //float totalTime = this->timers[ "total" ].getRealTime();
+
+      for ( auto const& [ key, val ] : this->timers )
+      {
+         logger.writeParameter( key, val.getRealTime() );
+         logger.writeParameter( key + "-average", val.getRealTime() / stepsTotal );
+         logger.writeParameter( key + "-percentage", val.getRealTime() );
+         logger.writeSeparator();
+      }
+
+      std::stringstream string;
+      string << str.rdbuf();
+      std::cout << string.str() << std::endl;
+   }
+
+protected:
+
+   std::map< std::string, TNL::Timer > timers;
+
+};
+
+
+} // SPH
+} // ParticleSystem
+} // TNL
+
