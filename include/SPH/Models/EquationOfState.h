53acef96f1867139f1f34cfac2e972098bd63f6b
Tomáš Halada <halada.tms@gmail.com>
2023-11-30 22:20:05 +0100

    Added linearized EOS.

--- include/SPH/Models/EquationOfState.h
+++ include/SPH/Models/EquationOfState.h
@@ -31,6 +31,33 @@ public:
    }
 };
 
+template< typename SPHCaseConfig >
+class TaitLinearizedWeaklyCompressibleEOS
+{
+public:
+   using RealType = typename SPHCaseConfig::RealType; //fix this
+
+   struct ParamsType
+   {
+     template< typename SPHState >
+     ParamsType( SPHState sphState )
+     : rho0( sphState.rho0 ),
+       c0( sphState.speedOfSound ) {}
+
+     const RealType rho0;
+     const RealType c0;
+   };
+
+   __cuda_callable__
+   static RealType
+   DensityToPressure( const RealType& rho, const ParamsType& params )
+   {
+      const RealType c0 = params.c0;
+      const RealType rho0 = params.rho0;
+      return c0 * c0 * ( rho - rho0 );
+   }
+};
+
 } // SPH
 } // ParticleSystem
 } // TNL
