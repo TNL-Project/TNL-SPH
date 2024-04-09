6796cababfd9b809856a7649b8b47ba2e8c6872a
Tomáš Halada <halada.tms@gmail.com>
2024-04-09 08:01:19 +0200

    Added density filter.

---
+++ include/SPH/Models/DensityFilters.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "../../Particles/neighborSearchLoop.h"
+#include "../SPHTraits.h"
+
+namespace TNL {
+namespace SPH {
+namespace DensityFilters {
+
+class None
+{
+   public:
+
+   template< typename FluidPointer, typename ModelParams >
+   static void
+   filterDensity( FluidPointer& fluid, ModelParams& modelParams ) {}
+};
+
+template< typename SPHCaseConfig, typename KernelFunction >
+class ShepardFilter
+{
+   public:
+   using DeviceType = typename SPHCaseConfig::DeviceType;
+   using SPHTraitsType = SPHFluidTraits< SPHCaseConfig >;
+   using LocalIndexType = typename SPHCaseConfig::LocalIndexType;
+   using GlobalIndexType = typename SPHCaseConfig::GlobalIndexType;
+   using RealType = typename SPHCaseConfig::RealType;
+   using VectorType = typename SPHTraitsType::VectorType;
+
+   template< typename Particles, typename FluidPointer, typename ModelParams >
+   static void
+   filterDensity( FluidPointer& fluid, ModelParams& modelParams )
+   {
+      typename Particles::NeighborsLoopParams searchInFluid( fluid->particles );
+
+      const auto view_points = fluid->particles->getPoints().getConstView();
+      auto view_rho = fluid->variables->rho.getView();
+      const RealType searchRadius = fluid->particles->getSearchRadius();
+      const RealType m = modelParams.mass;
+      const RealType h = modelParams.h;
+      const RealType rho0 = modelParams.rho0;
+
+      auto DensityFilter = [=] __cuda_callable__ ( LocalIndexType i,
+                                                   LocalIndexType j,
+                                                   VectorType& r_i,
+                                                   RealType* rho_i,
+                                                   RealType* gamma_i ) mutable
+      {
+         const VectorType r_j = view_points[ j ];
+         const VectorType r_ij = r_i - r_j;
+         const RealType drs = l2Norm( r_ij );
+         if (drs <= searchRadius )
+         {
+            const RealType rho_j = view_rho[ j ];
+            const RealType W = KernelFunction::W( drs, h );
+
+            *rho_i += W * m;
+            *gamma_i += W * m / rho_j;
+         }
+      };
+
+      auto particleLoop = [=] __cuda_callable__ ( LocalIndexType i ) mutable
+      {
+         const VectorType r_i = view_points[ i ];
+         RealType rho_i = 0.f;
+         RealType gamma_i = 0.f;
+         TNL::ParticleSystem::NeighborsLoop::exec( i, r_i, searchInFluid, DensityFilter, &rho_i,  &gamma_i );
+
+         if( gamma_i > 0.01f ){
+            //view_rho[ i ] = ( rho_i / gamma_i > rho0 ) ? ( rho_i / gamma_i ) : rho0;
+            view_rho[ i ] = rho_i / gamma_i;
+         }
+         else
+            view_rho[ i ] = rho0;
+      };
+      Algorithms::parallelFor< DeviceType >(
+            fluid->getFirstActiveParticle(), fluid->getLastActiveParticle() + 1, particleLoop );
+   }
+};
+
+
+} // ViscousTerms
+} // SPH
+} // TNL
+
