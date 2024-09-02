c480d1783fa32b35c8f6ecb56fb6e0962eed269c
Tomáš Halada <halada.tms@gmail.com>
2024-09-02 16:05:50 +0200

    WCSPH_BI 3D example draft.

---
+++ examples/WCSPH-BI/damBreak3D_WCSPH-BI/case.h
@@ -0,0 +1,92 @@
+#include <TNL/Config/parseCommandLine.h>
+#include <TNL/Config/parseINIConfigFile.h>
+#include <TNL/Logger.h>
+
+#include <SPH/configSetup.h>
+#include <SPH/configInit.h>
+#include "template/config.h"
+
+#include <SPH/Models/WCSPH_BI/control.h>
+
+int main( int argc, char* argv[] )
+{
+   // prepare client parameters
+   TNL::Config::ParameterContainer cliParams;
+   TNL::Config::ConfigDescription cliConfig;
+
+   // prepare sph parameters
+   TNL::Config::ParameterContainer parameters;
+   TNL::Config::ConfigDescription config;
+
+   try {
+      TNL::SPH::template initialize< Simulation >( argc, argv, cliParams, cliConfig, parameters, config );
+   }
+   catch ( ... ) {
+      return EXIT_FAILURE;
+   }
+
+   TNL::Logger log( 100, std::cout );
+   Simulation sph;
+   sph.init( parameters, log );
+   sph.writeProlog( log );
+
+   // Solver model:
+
+   //sph.init( parameters );
+   //sph.writeProlog( parameters );
+   //sph.exec();
+   //sph.writeEpilog( parameters );
+
+   // Library model:
+
+   while( sph.timeStepping.runTheSimulation() )
+   //while( sph.timeStepping.getStep() < 1 )
+   {
+      // search for neighbros
+      sph.timeMeasurement.start( "search" );
+      sph.performNeighborSearch( log );
+      sph.timeMeasurement.stop( "search" );
+      sph.writeLog( log, "Search...", "Done." );
+
+      // perform interaction with given model
+      sph.timeMeasurement.start( "interact" );
+      sph.interact();
+      sph.timeMeasurement.stop( "interact" );
+      sph.writeLog( log, "Interact...", "Done." );
+
+      // custom: no-penetration bc
+      BoundaryCorrection::boundaryCorrection( sph.fluid, sph.boundary, sph.modelParams );
+
+      //integrate
+      sph.timeMeasurement.start( "integrate" );
+      sph.integrator->integratStepVerlet( sph.fluid, sph.boundary, sph.timeStepping );
+      sph.timeMeasurement.stop( "integrate" );
+      sph.writeLog( log, "Integrate...", "Done." );
+
+      // output particle data
+      if( sph.timeStepping.checkOutputTimer( "save_results" ) )
+      {
+         /**
+          * Compute pressure from density.
+          * This is not necessary since we do this localy, if pressure is needed.
+          * It's useful for output anyway.
+          */
+         sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
+         sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
+
+         sph.save( log );
+      }
+
+      // check timers and if measurement or interpolation should be performed, is performed
+      sph.template measure< SPHDefs::KernelFunction, SPHDefs::EOS >( log );
+
+      // check timers and if snapshot should be done, is done
+      //sph.save( log );
+
+      // update time step
+      sph.timeStepping.updateTimeStep();
+   }
+
+   sph.writeEpilog( log );
+}
+
