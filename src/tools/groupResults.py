adfe293b207201819d6f273a64ada22c6400150d
Tomáš Halada <halada.tms@gmail.com>
2024-10-07 16:07:41 +0200

    Updated group tool.

--- src/tools/groupResults.py
+++ src/tools/groupResults.py
@@ -1,17 +1,20 @@
-from os import listdir
-from os.path import isfile, join
+import os
+from os import listdir, makedirs
+from os.path import isfile, join, exists
 from pathlib import Path
 from re import findall
 import configparser as cfp
 import numpy as np
 import json
 
-def create_series_file( identifier, snapshot_names, snapshot_timers ):
+def create_series_file( identifier, results_dir, snapshot_names, snapshot_timers ):
    n_snapshots = len( snapshot_names )
    files_list = [ {} for _ in range( n_snapshots ) ]
 
    for i in range( 0, n_snapshots ):
-       files_list[ i ][ "name" ] = snapshot_names[ i ]
+       snapshot_path = str( results_dir / "results_data" / snapshot_names[ i ])
+       print( snapshot_path )
+       files_list[ i ][ "name" ] = snapshot_path
        files_list[ i ][ "time" ] = snapshot_timers[ i ]
 
    series_file_dict = {
@@ -43,31 +46,50 @@ def load_case_measuretool_config( path = "" ):
 
     return measuretool_config
 
-def make_data_series_from_interpolation_planes( config, measuretool_config ):
-    for p in range( 1, int( config[ 'main' ][ 'interpolation-planes-count' ] ) + 1 ):
-        plane_key = f'plane-{p}-identifier'
-        plane_identifier= measuretool_config[ 'main' ][ plane_key ]
-        print( plane_identifier )
+def select_data_and_build_series( files, results_dir, output_identifier ):
+        # NOTE: This is numpy array only due to sorting
+        plane_file_names_subroup = np.array( [ f for f in files if output_identifier in f ] )
+        if np.size( plane_file_names_subroup ) == 0:
+            return
 
-        # NOTE: This is numpy array only due to
-        plane_file_names_subroup = np.array( [ f for f in files if plane_identifier in f ])
-        plane_timers_subgroup = np.array( [ findall( r'[\d]*[.][\d]+', f.replace( plane_identifier, "" ) ) for f in plane_file_names_subroup ], dtype=float )
+        # NOTE: Version to find only decimals: r'[\d]*[.][\d]+'
+        plane_timers_subgroup = np.array( [ findall( r'[-+]?(?:\d*\.*\d+)', f.replace( output_identifier, "" ) ) for f in plane_file_names_subroup ], dtype=float )
 
-        ## sort the arra based on saved timers
+        # sort the array based on saved timers
         idx_sort = plane_timers_subgroup.ravel().argsort()
         plane_file_names_subroup = plane_file_names_subroup[ idx_sort ]
         plane_timers_subgroup = plane_timers_subgroup[ idx_sort ]
 
-        create_series_file( plane_identifier, plane_file_names_subroup.ravel(), plane_timers_subgroup.ravel() )
+        create_series_file( output_identifier, results_dir, plane_file_names_subroup.ravel(), plane_timers_subgroup.ravel() )
 
-def make_data_series():
-    path = Path(__file__).parent
-    files = [ f for f in listdir( path ) if isfile( join( path, f ) ) ]
+        # move source data folder with raw files
+        for plane_file in plane_file_names_subroup:
+            os.rename( results_dir / Path( plane_file ),  results_dir / "results_data" / Path( plane_file ) )
+
+def make_data_series_from_interpolation_planes( files, results_dir, config, measuretool_config ):
+    for p in range( 1, int( config[ 'main' ][ 'interpolation-planes-count' ] ) + 1 ):
+        plane_key = f'plane-{p}-identifier'
+        plane_identifier= measuretool_config[ 'main' ][ plane_key ]
+        select_data_and_build_series( files, results_dir, plane_identifier )
+
+def make_data_series_from_particles( files, results_dir, config ):
+    output_identifiers = [ 'fluid', 'boundary', 'inlet', 'outlet', 'grid' ]
+    for output_identifier in output_identifiers:
+        select_data_and_build_series( files, results_dir, output_identifier )
+
+def make_data_series( example_dir ):
+    results_dir = example_dir / "results"
+    files = [ f for f in listdir( results_dir ) if isfile( join( results_dir, f ) ) ]
+
+    results_raw_data_dir = results_dir / "results_data"
+    if not os.path.exists( results_raw_data_dir ):
+        os.makedirs( results_raw_data_dir )
 
     config = load_case_config()
     measuretool_config = load_case_measuretool_config()
 
-    make_data_series_from_interpolation_planes( config, measuretool_config )
+    make_data_series_from_particles( files, results_dir, config )
+    make_data_series_from_interpolation_planes( files, results_dir, config, measuretool_config )
 
 if __name__ == "__main__":
     make_data_series()
