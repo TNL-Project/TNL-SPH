f6473af226e248913e80461dcfbcec588c206849
Tomáš Halada <halada.tms@gmail.com>
2024-10-24 14:24:52 +0200

    Seprated time loops.

--- examples/WCSPH-BI/damBreak2D_WCSPH-BI/case.h
+++ examples/WCSPH-BI/damBreak2D_WCSPH-BI/case.h
@@ -7,14 +7,12 @@
 #include "template/config.h"
 
 #include <SPH/Models/WCSPH_BI/control.h>
-#include <SPH/DefaultTimeLoops.h>
 
-template< typename FluidPointer,
-          typename BoudaryPointer,
-          typename TimeScheme = typename SPHDefs::IntegrationScheme,
-          std::enable_if_t< std::is_same_v< TimeScheme, TNL::SPH::IntegrationSchemes::SymplecticVerletScheme< typename SPHDefs::SPHConfig > >, bool > Enabled = true >
+template< typename Simulation,
+          typename IntegrationScheme = typename Simulation::ModelParams::IntegrationScheme,
+          typename std::enable_if_t< std::is_same_v< IntegrationScheme, TNL::SPH::IntegrationSchemes::SymplecticVerletScheme< typename Simulation::SPHConfig > >, bool > Enabled = true >
 void
-exec( Simulation& sph, TNL::Logger log )
+exec( Simulation& sph, TNL::Logger& log )
 {
    while( sph.timeStepping.runTheSimulation() )
    {
@@ -59,14 +57,12 @@ exec( Simulation& sph, TNL::Logger log )
       sph.writeLog( log, "Interact...", "Done." );
 
       // output particle data
-      if( sph.timeStepping.checkOutputTimer( "save_results" ) )
-      {
+      if( sph.timeStepping.checkOutputTimer( "save_results" ) ){
          // compute pressure from density
          sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
          sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
          sph.save( log );
       }
-
       // check timers and if measurement or interpolation should be performed, is performed
       sph.template measure< SPHDefs::KernelFunction, SPHDefs::EOS >( log );
 
@@ -75,12 +71,11 @@ exec( Simulation& sph, TNL::Logger log )
    }
 }
 
-template< typename FluidPointer,
-          typename BoudaryPointer,
-          typename TimeScheme = typename SPHDefs::IntegrationScheme,
-          std::enable_if_t< std::is_same_v< TimeScheme, TNL::SPH::IntegrationSchemes::MidpointScheme< typename SPHDefs::SPHConfig > >, bool > Enabled = true >
+template< typename Simulation,
+          typename IntegrationScheme = typename Simulation::ModelParams::IntegrationScheme,
+          typename std::enable_if_t< std::is_same_v< IntegrationScheme, TNL::SPH::IntegrationSchemes::MidpointScheme< typename Simulation::SPHConfig > >, bool > Enabled = true >
 void
-exec( Simulation& sph, TNL::Logger log )
+exec( Simulation& sph, TNL::Logger& log )
 {
    // search for neighbros
    sph.timeMeasurement.start( "search" );
@@ -96,25 +91,6 @@ exec( Simulation& sph, TNL::Logger log )
 
    while( sph.timeStepping.runTheSimulation() ){
 
-      //int midpointIteration = 0;
-      //const int midpointMaxInterations = 50; //10 bdflt
-      //const RealType residuaTolerance = 1.e-6 * 264.0753900000003f;
-      //RealType midpointRelaxCoef = 0;
-      //const RealType midpointRelaxCoef_0 = midpointRelaxCoef;
-      //const RealType residaMinimualDecay = 1.f / 5.f;
-      //RealType residuaPrevious = 1.f;
-      //const RealType midpointRelaxCoefIncrement = 0.25f;
-
-      const int midpointMaxInterations = 3000; //10 bdflt
-      const RealType residuaTolerance = 1e-4f; // 1.e-6 * 264.0753900000003f
-      RealType midpointRelaxCoef = 0.;
-      const RealType midpointRelaxCoef_0 = 0;
-      const RealType residaMinimualDecay = 1.f / 2.f;
-      RealType residuaPrevious = 1.f;
-      //const RealType midpointRelaxCoefIncrement = 0.25;
-      //const RealType midpointRelaxCoefIncrement = 0.1;
-      const RealType midpointRelaxCoefIncrement = 0.001;
-
       sph.timeMeasurement.start( "integrate" );
       sph.integrator->predictor( sph.timeStepping.getTimeStep(), sph.fluid );
       sph.timeMeasurement.stop( "integrate" );
@@ -124,7 +100,7 @@ exec( Simulation& sph, TNL::Logger log )
       float residualPrevious = 0.f;
       float midpointRelaxCoef = sph.modelParams.midpointRelaxCoef;
 
-      while( midpointIteration < midpointMaxInterations ) {
+      while( midpointIteration < sph.modelParams.midpointMaxInterations ) {
 
          // backup derivatives
          sph.fluid->integratorVariables->drhodt_in = sph.fluid->variables->drho;
@@ -154,13 +130,13 @@ exec( Simulation& sph, TNL::Logger log )
 
          // compute residua
          sph.timeMeasurement.start( "integrate" );
-         const RealType residual = sph.integrator->midpointResiduals( sph.fluid, sph.modelParams );
+         const float residual = sph.integrator->midpointResiduals( sph.fluid, sph.modelParams );
          sph.timeMeasurement.stop( "integrate" );
          sph.writeLog( log, "Integrate: compute residuals...", "Done." );
 
          // stop midpoint iterations
-         if( residual < residuaTolerance )
-            midpointIteration = midpointMaxInterations;
+         if( residual < sph.modelParams.residuaTolerance )
+            midpointIteration = sph.modelParams.midpointMaxInterations;
          // constrol residua decay
          if( midpointIteration > 0 )
             if( residual / residualPrevious > sph.modelParams.residaMinimualDecay )
@@ -174,7 +150,7 @@ exec( Simulation& sph, TNL::Logger log )
          sph.timeMeasurement.stop( "integrate" );
          sph.writeLog( log, "Integrate: relax...", "Done." );
 
-         std::cout  << "Midpoint iteractions: " << midpointIteration << " residua: " << maxResidua << " relax coef: " << midpointRelaxCoef << std::endl;
+         std::cout  << "Midpoint iteractions: " << midpointIteration << " residua: " << residual << " relax coef: " << midpointRelaxCoef << std::endl;
          midpointIteration++;
       }
 
@@ -186,35 +162,67 @@ exec( Simulation& sph, TNL::Logger log )
       sph.timeMeasurement.stop( "integrate" );
       sph.writeLog( log, "Integrate: corrector...", "Done." );
 
-     // output particle data
-     if( sph.timeStepping.checkOutputTimer( "save_results" ) )
-     {
-        /**
-         * Compute pressure from density.
-         * This is not necessary since we do this localy, if pressure is needed.
-         * It's useful for output anyway.
-         */
-        sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
-        sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
+      // output particle data
+      if( sph.timeStepping.checkOutputTimer( "save_results" ) ){
+         // compute pressure from density
+         sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
+         sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
 
-        sph.save( log );
-     }
+         sph.save( log );
+      }
+      // check timers and if measurement or interpolation should be performed, is performed
+      sph.template measure< SPHDefs::KernelFunction, SPHDefs::EOS >( log );
 
-         //// search for neighbros
-         //sph.timeMeasurement.start( "search" );
-         //sph.performNeighborSearch( log );
-         //sph.timeMeasurement.stop( "search" );
-         //sph.writeLog( log, "Search...", "Done." );
-
-         //// perform interaction with given model
-         //sph.timeMeasurement.start( "interact" );
-         //sph.interact(); //TODO: What about BC conditions?
-         //sph.timeMeasurement.stop( "interact" );
-         //sph.writeLog( log, "Interact...", "Done." );
-
+      // update time step
       sph.timeStepping.updateTimeStep();
    }
+}
 
+template< typename Simulation,
+          typename IntegrationScheme = typename Simulation::ModelParams::IntegrationScheme,
+          typename std::enable_if_t< std::is_same_v< IntegrationScheme, TNL::SPH::IntegrationSchemes::RK45Scheme< typename Simulation::SPHConfig > >, bool > Enabled = true >
+void
+exec( Simulation& sph, TNL::Logger& log )
+{
+   while( sph.timeStepping.runTheSimulation() ){
+      for( int predictorStep = 0; predictorStep < 4; predictorStep++ ){
+         // predictor step
+         sph.timeMeasurement.start( "integrate" );
+         sph.integrator->predictor( sph.timeStepping.getTimeStep(), sph.fluid, predictorStep );
+         sph.timeMeasurement.stop( "integrate" );
+         sph.writeLog( log, "Integrate: predictor...", "Done." );
+
+         // search for neighbros
+         sph.timeMeasurement.start( "search" );
+         sph.performNeighborSearch( log );
+         sph.timeMeasurement.stop( "search" );
+         sph.writeLog( log, "Search...", "Done." );
+
+         // perform interaction with given model
+         sph.timeMeasurement.start( "interact" );
+         sph.interact(); //TODO: What about BC conditions?
+         sph.timeMeasurement.stop( "interact" );
+         sph.writeLog( log, "Interact...", "Done." );
+
+         // custom: no-penetration bc
+         //BoundaryCorrection::boundaryCorrection( sph.fluid, sph.boundary, sph.modelParams, sph.timeStepping.getTimeStep() );
+      }
+
+      // predictor step
+      sph.timeMeasurement.start( "integrate" );
+      sph.integrator->corrector( sph.timeStepping.getTimeStep(), sph.fluid );
+      sph.timeMeasurement.stop( "integrate" );
+      sph.writeLog( log, "Integrate: predictor...", "Done." );
+
+       // output particle data
+       if( sph.timeStepping.checkOutputTimer( "save_results" ) ) {
+          // compute pressure from density
+          sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
+          sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
+
+          sph.save( log );
+       }
+      sph.timeStepping.updateTimeStep();
    }
 }
 
@@ -240,217 +248,6 @@ int main( int argc, char* argv[] )
    sph.init( parameters, log );
    sph.writeProlog( log );
    exec( sph, log );
-
-
-   sph.fluid->variables->a = 0.f;
-   sph.fluid->variables->drho = 0.f;
-
-   if constexpr ( std::is_same_v< SPHDefs::IntegrationScheme, TNL::SPH::IntegrationSchemes::MidpointScheme< typename SPHDefs::SPHConfig > > ){
-      using RealType = typename Simulation::RealType;
-
-      while( sph.timeStepping.runTheSimulation() )
-      {
-         std::cout << "Step: " << sph.timeStepping.getStep() << std::endl;
-
-         //int midpointIteration = 0;
-         //const int midpointMaxInterations = 50; //10 bdflt
-         //const RealType residuaTolerance = 1.e-6 * 264.0753900000003f;
-         //RealType midpointRelaxCoef = 0;
-         //const RealType midpointRelaxCoef_0 = midpointRelaxCoef;
-         //const RealType residaMinimualDecay = 1.f / 5.f;
-         //RealType residuaPrevious = 1.f;
-         //const RealType midpointRelaxCoefIncrement = 0.25f;
-
-         int midpointIteration = 0;
-         const int midpointMaxInterations = 3000; //10 bdflt
-         const RealType residuaTolerance = 1e-4f; // 1.e-6 * 264.0753900000003f
-         RealType midpointRelaxCoef = 0.;
-         const RealType midpointRelaxCoef_0 = 0;
-         const RealType residaMinimualDecay = 1.f / 2.f;
-         RealType residuaPrevious = 1.f;
-         //const RealType midpointRelaxCoefIncrement = 0.25;
-         //const RealType midpointRelaxCoefIncrement = 0.1;
-         const RealType midpointRelaxCoefIncrement = 0.001;
-
-         //// search for neighbros
-         //sph.timeMeasurement.start( "search" );
-         //sph.performNeighborSearch( log );
-         //sph.timeMeasurement.stop( "search" );
-         //sph.writeLog( log, "Search...", "Done." );
-
-         //// perform interaction with given model
-         //sph.timeMeasurement.start( "interact" );
-         //sph.interact(); //TODO: What about BC conditions?
-         //sph.timeMeasurement.stop( "interact" );
-         //sph.writeLog( log, "Interact...", "Done." );
-
-         //THIS IS TERRIBLY WRONG! THIS OVERWRITE THE LAST ITERAION FIELDS
-         //sph.fluid->variables->drho = sph.fluid->integratorVariables->drhodt_in;
-         //sph.fluid->variables->a = sph.fluid->integratorVariables->dvdt_in;
-
-         sph.timeMeasurement.start( "integrate" );
-         sph.integrator->predictor( sph.timeStepping.getTimeStep(), sph.fluid );
-         sph.timeMeasurement.stop( "integrate" );
-         sph.writeLog( log, "Integrate: predictor...", "Done." );
-
-         //THIS IS IMHO NOT NECESSARY
-         sph.fluid->variables->drho = sph.fluid->integratorVariables->drhodt_in;
-         sph.fluid->variables->a = sph.fluid->integratorVariables->dvdt_in;
-
-         while( midpointIteration < midpointMaxInterations )
-         {
-            //
-            sph.fluid->integratorVariables->drhodt_in = sph.fluid->variables->drho;
-            sph.fluid->integratorVariables->dvdt_in = sph.fluid->variables->a;
-
-            // update inner loop variables
-            sph.timeMeasurement.start( "integrate" );
-            sph.integrator->midpointUpdateVariables( sph.timeStepping.getTimeStep(), sph.fluid );
-            //sph.integrator->midpointUpdatePositions( sph.timeStepping.getTimeStep(), sph.fluid );
-            sph.timeMeasurement.stop( "integrate" );
-            sph.writeLog( log, "Integrate: midpoint update...", "Done." );
-
-            // search for neighbros
-            sph.timeMeasurement.start( "search" );
-            sph.performNeighborSearch( log, true );
-            sph.timeMeasurement.stop( "search" );
-            sph.writeLog( log, "Search...", "Done." );
-
-            // perform interaction with given model
-            sph.timeMeasurement.start( "interact" );
-            sph.interact(); //TODO: What about BC conditions?
-            sph.timeMeasurement.stop( "interact" );
-            sph.writeLog( log, "Interact...", "Done." );
-
-            // custom: no-penetration bc
-            BoundaryCorrection::boundaryCorrection( sph.fluid, sph.boundary, sph.modelParams, sph.timeStepping.getTimeStep() );
-
-            // compute residua
-            sph.timeMeasurement.start( "integrate" );
-            sph.integrator->midpointResiduals( sph.fluid, sph.modelParams );
-            sph.timeMeasurement.stop( "integrate" );
-            sph.writeLog( log, "Integrate: compute residuals...", "Done." );
-
-            // backup residua and get new maxima
-            sph.timeMeasurement.start( "integrate" );
-            const RealType maxResidua = sph.integrator->getMaxResidua( sph.fluid );
-            sph.timeMeasurement.stop( "integrate" );
-            sph.writeLog( log, "Integrate: reduce residuals...", "Done." );
-            if( maxResidua < residuaTolerance )
-               midpointIteration = midpointMaxInterations;
-
-            // constrol residua decay
-            if( midpointIteration > 0)
-               if( maxResidua / residuaPrevious > residaMinimualDecay )
-                   midpointRelaxCoef = midpointRelaxCoefIncrement + ( 1.0 - midpointRelaxCoefIncrement ) * midpointRelaxCoef; //TODO: Not sure here
-
-            // backup residua
-            residuaPrevious = maxResidua;
-
-            // relax
-            sph.timeMeasurement.start( "integrate" );
-            if( midpointIteration == 0 )
-               sph.integrator->relax( sph.fluid, midpointRelaxCoef_0 );
-            else if( midpointIteration == midpointMaxInterations )
-               sph.integrator->relax( sph.fluid, 0.f );
-            else
-               sph.integrator->relax( sph.fluid, midpointRelaxCoef );
-            sph.timeMeasurement.stop( "integrate" );
-            sph.writeLog( log, "Integrate: relax...", "Done." );
-
-            std::cout  << "Midpoint iteractions: " << midpointIteration << " residua: " << maxResidua << " relax coef: " << midpointRelaxCoef << std::endl;
-            midpointIteration++;
-         }
-         //midpointIteration = 0;
-
-         sph.timeMeasurement.start( "integrate" );
-         if( sph.timeStepping.getStep() == 0 )
-            sph.integrator->corrector( 0, sph.fluid );
-         else
-            sph.integrator->corrector( sph.timeStepping.getTimeStep(), sph.fluid );
-         sph.timeMeasurement.stop( "integrate" );
-         sph.writeLog( log, "Integrate: corrector...", "Done." );
-
-        // output particle data
-        if( sph.timeStepping.checkOutputTimer( "save_results" ) )
-        {
-           /**
-            * Compute pressure from density.
-            * This is not necessary since we do this localy, if pressure is needed.
-            * It's useful for output anyway.
-            */
-           sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
-           sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
-
-           sph.save( log );
-        }
-
-            //// search for neighbros
-            //sph.timeMeasurement.start( "search" );
-            //sph.performNeighborSearch( log );
-            //sph.timeMeasurement.stop( "search" );
-            //sph.writeLog( log, "Search...", "Done." );
-
-            //// perform interaction with given model
-            //sph.timeMeasurement.start( "interact" );
-            //sph.interact(); //TODO: What about BC conditions?
-            //sph.timeMeasurement.stop( "interact" );
-            //sph.writeLog( log, "Interact...", "Done." );
-
-         sph.timeStepping.updateTimeStep();
-      }
-
-   }
-
-   //if constexpr ( std::is_same_v< SPHDefs::IntegrationScheme, TNL::SPH::IntegrationSchemes::RK45Scheme< typename SPHDefs::SPHConfig > > ){
-   //   while( sph.timeStepping.runTheSimulation() ){
-   //      std::cout << "Time: " << sph.timeStepping.getTime() << " step: " << sph.timeStepping.getStep() << std::endl;
-
-   //      for( int predictorStep = 0; predictorStep < 4; predictorStep++ ){
-   //         // predictor step
-   //         sph.timeMeasurement.start( "integrate" );
-   //         sph.integrator->predictor( sph.timeStepping.getTimeStep(), sph.fluid, predictorStep );
-   //         sph.timeMeasurement.stop( "integrate" );
-   //         sph.writeLog( log, "Integrate: predictor...", "Done." );
-
-   //         // search for neighbros
-   //         sph.timeMeasurement.start( "search" );
-   //         sph.performNeighborSearch( log );
-   //         sph.timeMeasurement.stop( "search" );
-   //         sph.writeLog( log, "Search...", "Done." );
-
-   //         // perform interaction with given model
-   //         sph.timeMeasurement.start( "interact" );
-   //         sph.interact(); //TODO: What about BC conditions?
-   //         sph.timeMeasurement.stop( "interact" );
-   //         sph.writeLog( log, "Interact...", "Done." );
-   //         // custom: no-penetration bc
-   //         //BoundaryCorrection::boundaryCorrection( sph.fluid, sph.boundary, sph.modelParams, sph.timeStepping.getTimeStep() );
-   //      }
-
-   //      // predictor step
-   //      sph.timeMeasurement.start( "integrate" );
-   //      sph.integrator->corrector( sph.timeStepping.getTimeStep(), sph.fluid );
-   //      sph.timeMeasurement.stop( "integrate" );
-   //      sph.writeLog( log, "Integrate: predictor...", "Done." );
-
-   //       // output particle data
-   //       if( sph.timeStepping.checkOutputTimer( "save_results" ) )
-   //       {
-   //          /**
-   //           * Compute pressure from density.
-   //           * This is not necessary since we do this localy, if pressure is needed.
-   //           * It's useful for output anyway.
-   //           */
-   //          sph.model.computePressureFromDensity( sph.fluid, sph.modelParams );
-   //          sph.model.computePressureFromDensity( sph.boundary, sph.modelParams );
-
-   //          sph.save( log );
-   //       }
-   //      sph.timeStepping.updateTimeStep();
-   //   }
-   //}
-
    sph.writeEpilog( log );
 }
 
