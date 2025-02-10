86dcf6c49e50b594b2b7028ac4ef0dbea65754d0
Tomáš Halada <halada.tms@gmail.com>
2025-02-10 17:12:47 +0100

    Added empty NBC-PST for EB.

--- include/SPH/shared/ElasticBounce.h
+++ include/SPH/shared/ElasticBounce.h
@@ -19,89 +19,118 @@ public:
    using RealType = typename SPHTraitsType::RealType;
    using VectorType = typename SPHTraitsType::VectorType;
 
-template< typename FluidPointer, typename BoudaryPointer, typename SPHState >
-static void
-boundaryCorrection( FluidPointer& fluid,
-                    BoudaryPointer& boundary,
-                    SPHState& sphState,
-                    const RealType& dt )
-{
-   /* PARTICLES AND NEIGHBOR SEARCH ARRAYS */
-   GlobalIndexType numberOfParticles = fluid->particles->getNumberOfParticles();
-   const RealType searchRadius = fluid->particles->getSearchRadius();
-
-   typename ParticleSystem::NeighborsLoopParams searchInBound( boundary->particles );
-
-   /* CONSTANT VARIABLES */
-   const RealType dp = sphState.dp;
-   const RealType r_box = sphState.r_boxFactor * dp;
-   const RealType minimalDistanceFactor = sphState.minimalDistanceFactor;
-   const RealType elasticFactor = sphState.elasticFactor;
-
-   /* VARIABLES AND FIELD ARRAYS */
-   const auto view_points = fluid->particles->getPoints().getView();
-   auto view_v = fluid->variables->v.getView();
-   auto view_a = fluid->variables->a.getView();
-
-   const auto view_points_bound = boundary->particles->getPoints().getView();
-   const auto view_v_bound = boundary->variables->v.getView();
-   const auto view_n_bound = boundary->variables->n.getView();
-
-   auto elasticBounce = [=] __cuda_callable__ ( LocalIndexType i, LocalIndexType j,
-         VectorType& r_i, VectorType* ve_i, VectorType* ae_i ) mutable
+   template< typename FluidPointer, typename BoudaryPointer, typename SPHState >
+   static void
+   boundaryCorrection( FluidPointer& fluid,
+                       BoudaryPointer& boundary,
+                       SPHState& sphState,
+                       const RealType& dt )
    {
-      const VectorType r_j = view_points_bound[ j ];
-      const VectorType r_ji = r_j - r_i;
-      const RealType drs = l2Norm( r_ji );
-      if (drs <= searchRadius )
+      /* PARTICLES AND NEIGHBOR SEARCH ARRAYS */
+      GlobalIndexType numberOfParticles = fluid->particles->getNumberOfParticles();
+      const RealType searchRadius = fluid->particles->getSearchRadius();
+
+      typename ParticleSystem::NeighborsLoopParams searchInBound( boundary->particles );
+
+      /* CONSTANT VARIABLES */
+      const RealType dp = sphState.dp;
+      const RealType r_box = sphState.r_boxFactor * dp;
+      const RealType minimalDistanceFactor = sphState.minimalDistanceFactor;
+      const RealType elasticFactor = sphState.elasticFactor;
+
+      /* VARIABLES AND FIELD ARRAYS */
+      const auto view_points = fluid->particles->getPoints().getView();
+      auto view_v = fluid->variables->v.getView();
+      auto view_a = fluid->variables->a.getView();
+
+      const auto view_points_bound = boundary->particles->getPoints().getView();
+      const auto view_v_bound = boundary->variables->v.getView();
+      const auto view_n_bound = boundary->variables->n.getView();
+
+      auto elasticBounce = [=] __cuda_callable__ ( LocalIndexType i, LocalIndexType j,
+            VectorType& r_i, VectorType* ve_i, VectorType* ae_i ) mutable
       {
-         const VectorType v_j = view_v_bound[ j ];
-         const VectorType n_j = ( -1.f ) * view_n_bound[ j ];
+         const VectorType r_j = view_points_bound[ j ];
+         const VectorType r_ji = r_j - r_i;
+         const RealType drs = l2Norm( r_ji );
+         if (drs <= searchRadius )
+         {
+            const VectorType v_j = view_v_bound[ j ];
+            const VectorType n_j = ( -1.f ) * view_n_bound[ j ];
 
-         const RealType r0 = ( r_ji, n_j );
-         // Particle is behind the wall
-         if( r0 < 0.f )
-            return;
+            const RealType r0 = ( r_ji, n_j );
+            // Particle is behind the wall
+            if( r0 < 0.f )
+               return;
 
-         const VectorType r_ji_box = r_ji - r0 * n_j;
-         // Particle is too far from boundary element
-         if( ( r_ji_box, r_ji_box ) >= r_box * r_box )
-            return;
+            const VectorType r_ji_box = r_ji - r0 * n_j;
+            // Particle is too far from boundary element
+            if( ( r_ji_box, r_ji_box ) >= r_box * r_box )
+               return;
 
-         const RealType v_n = ( *ve_i - v_j, n_j );
-         const RealType dvdt_n = ( *ae_i, n_j ); //TODO: include dvdt_j
-         const RealType r_n = dt * v_n + 0.5f * dt * dt *  dvdt_n ;
+            const RealType v_n = ( *ve_i - v_j, n_j );
+            const RealType dvdt_n = ( *ae_i, n_j ); //TODO: include dvdt_j
+            const RealType r_n = dt * v_n + 0.5f * dt * dt *  dvdt_n ;
 
-         // Particle is already running away from boundary
-         if( r_n < 0.f )
-            return;
+            // Particle is already running away from boundary
+            if( r_n < 0.f )
+               return;
 
-         // Reflect the particle
-         if( r0 - r_n <= minimalDistanceFactor * dp ){
-            *ve_i = ( *ve_i ) - ( 1.f + elasticFactor ) * v_n * n_j;
-            *ae_i = ( *ae_i ) - ( 1.f + elasticFactor ) * dvdt_n * n_j;
+            // Reflect the particle
+            if( r0 - r_n <= minimalDistanceFactor * dp ){
+               *ve_i = ( *ve_i ) - ( 1.f + elasticFactor ) * v_n * n_j;
+               *ae_i = ( *ae_i ) - ( 1.f + elasticFactor ) * dvdt_n * n_j;
+            }
          }
-      }
-   };
+      };
 
-   auto particleLoop = [=] __cuda_callable__ ( LocalIndexType i ) mutable
+      auto particleLoop = [=] __cuda_callable__ ( LocalIndexType i ) mutable
+      {
+         const VectorType r_i = view_points[ i ];
+         const VectorType v_i = view_v[ i ];
+         const VectorType dvdt_i = view_a[ i ];
+
+         VectorType ve_i = v_i;
+         VectorType ae_i = dvdt_i;
+
+         ParticleSystem::NeighborsLoopAnotherSet::exec( i, r_i, searchInBound, elasticBounce, &ve_i, &ae_i );
+
+         view_v[ i ] = ve_i;
+         view_a[ i ] = ae_i;
+
+      };
+      Algorithms::parallelFor< DeviceType >( 0, numberOfParticles, particleLoop );
+   }
+
+   template< typename FluidPointer, typename BoudaryPointer, typename SPHState >
+   static void
+   boundaryCorrectionPST( FluidPointer& fluid,
+                          BoudaryPointer& boundary,
+                          SPHState& sphState,
+                          const RealType& dt )
    {
-      const VectorType r_i = view_points[ i ];
-      const VectorType v_i = view_v[ i ];
-      const VectorType dvdt_i = view_a[ i ];
+      GlobalIndexType numberOfParticles = fluid->particles->getNumberOfParticles();
+      const RealType searchRadius = fluid->particles->getSearchRadius();
+      typename ParticleSystem::NeighborsLoopParams searchInBound( boundary->particles );
 
-      VectorType ve_i = v_i;
-      VectorType ae_i = dvdt_i;
+      const RealType m = sphState.mass;
+      const RealType r_boxFactor = sphState.r_boxFactor;
 
-      ParticleSystem::NeighborsLoopAnotherSet::exec( i, r_i, searchInBound, elasticBounce, &ve_i, &ae_i );
+      auto view_points = fluid->particles->getPoints().getView();
+      const auto view_rho = fluid->variables->rho.getConstView();
+      const auto view_points_bound = boundary->particles->getPoints().getView();
+      const auto view_n_bound = boundary->variables->n.getConstView();
+      const auto view_elementSize_bound = boundary->variables->elementSize.getConstView();
 
-      view_v[ i ] = ve_i;
-      view_a[ i ] = ae_i;
+      auto pstLoop = [=] __cuda_callable__ ( LocalIndexType i,
+                                             LocalIndexType j,
+                                             VectorType& r_i,
+                                             RealType& Ri,
+                                             VectorType* r_i_mod ) mutable
+      {
+         // do nothing
+      };
 
-   };
-   Algorithms::parallelFor< DeviceType >( 0, numberOfParticles, particleLoop );
-
-}
 };
 
 template< typename ParticleSystem, typename SPHConfig>
