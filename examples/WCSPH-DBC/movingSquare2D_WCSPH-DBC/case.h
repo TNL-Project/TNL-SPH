ef7b694b5e9a9c43164b71b137c6af8e6a8345e8
Tomáš Halada <halada.tms@gmail.com>
2025-12-23 18:18:28 +0100

    Added spheric 06 example to WCSPH-DBC model

---
+++ examples/WCSPH-DBC/movingSquare2D_WCSPH-DBC/case.h
@@ -0,0 +1,53 @@
+#include "template/config.h"
+#include "template/userCodedFunctions.h"
+
+template< typename Simulation >
+requires std::is_same_v<
+    typename Simulation::ModelParams::IntegrationScheme,
+    TNL::SPH::IntegrationSchemes::VerletScheme< typename SPHDefs::SPHConfig >
+>
+void exec( Simulation& sph )
+{
+   userCodedFunctions::CustomMotion motion( "template/Motion_Body.dat" );
+   EnergyMonitor energyMonitor;
+   energyMonitor.init( sph.fluid, true );
+
+   while( sph.timeStepping.runTheSimulation() )
+   {
+      // FEATURE: asssing square motion
+      motion.assignMotion( sph.boundary, sph.timeStepping );
+
+      // search for neighbros
+      sph.performNeighborSearch( true );
+
+      // perform interaction with given model
+      sph.interact();
+
+      // FEATURE: monitor energy levels
+      energyMonitor.computeEnergyDerivatives( sph.fluid, sph.modelParams );
+      energyMonitor.integrate( sph.timeStepping.getTimeStep() );
+
+      //integrate
+      sph.integrateVerletStep( SPHDefs::BCType::integrateInTime() );
+
+      // output particle data
+      sph.makeSnapshot();
+      energyMonitor.output( sph.outputDirectory + "/energy.dat", sph.timeStepping.getStep(), sph.timeStepping.getTime() );
+
+      // check timers and if measurement or interpolation should be performed, is performed
+      sph.measure();
+
+      // update time step
+      sph.timeStepping.updateTimeStep();
+   }
+}
+
+int main( int argc, char* argv[] )
+{
+   Simulation sph;
+   sph.init( argc, argv );
+   sph.writeProlog();
+   exec( sph );
+   sph.writeEpilog();
+}
+
