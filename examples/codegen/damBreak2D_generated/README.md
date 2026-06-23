afac27cf30b7de7757dd3e36b572db3884d00336
Tomáš Halada <halada.tms@gmail.com>
2026-06-23 10:35:52 +0200

    Added missing codegen example

---
+++ examples/codegen/damBreak2D_generated/README.md
@@ -0,0 +1,9 @@
+## 2D dam break example
+
+Two-dimensional implementation of the standard SPH example, compared with experimental data provided in following publication:
+
+- L. Lobovsky et al. [Experimental investigation of dynamic pressure loads during dam break](https://doi.org/10.1016/j.jfluidstructs.2014.03.009). In: Journal of Fluids and Structures 48 (Aug. 2013).
+
+#### Scheme of the example
+
+![dmBreak2D_scheme](../../resources/damBreak2D/damBreak_scheme.png "2D dam break example scheme")
---
+++ examples/codegen/damBreak2D_generated/case.h
@@ -0,0 +1,36 @@
+#include "template/config.h"
+
+int main( int argc, char* argv[] )
+{
+   Simulation sph;
+   sph.init( argc, argv );
+   sph.writeProlog();
+
+   while( sph.timeStepping.runTheSimulation() )
+   {
+      std::cout << "Step: " << sph.timeStepping.getStep() << ", time: " << sph.timeStepping.getTime() << std::endl;
+      // search for neighbros
+      sph.performNeighborSearch();
+
+      // perform interaction with given model
+      sph.interact();
+
+      // in case of variable time step, compute the step
+      sph.computeTimeStep();
+
+      // make integration step with Verlet scheme
+      sph.integrateVerletStep( SPHDefs::BCType::integrateInTime() );
+
+      // check timers and if output should be performed, it is performed
+      sph.makeSnapshot();
+
+      // check timers and if measurement or interpolation should be performed, it is performed
+      sph.measure();
+
+      // update time step
+      sph.updateTime();
+   }
+
+   sph.writeEpilog();
+}
+
